af27161e32565d7a1b6faa945fc2a0d23d6e6dfe
Sergey <sergeyayya@gmail.com>
2023-05-30 18:09:03 +0200

    Revert 'puma fix'

--- config/puma.rb
+++ config/puma.rb
@@ -17,35 +17,35 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 #
 port ENV.fetch("PORT") { 3000 }
 
-# Change to match your CPU core count
-workers 2
+# # Change to match your CPU core count
+# workers 2
 
-# Min and Max threads per worker
-threads 1, 6
+# # Min and Max threads per worker
+# threads 1, 6
 
-app_dir = '/root/match_wizard'
-shared_dir = "#{app_dir}/shared"
+# app_dir = '/root/match_wizard'
+# shared_dir = "#{app_dir}/shared"
 
-# Default to production
-rails_env = ENV['RAILS_ENV'] || "production"
-environment rails_env
+# # Default to production
+# rails_env = ENV['RAILS_ENV'] || "production"
+# environment rails_env
 
-# Set up socket location
-bind "unix://#{shared_dir}/sockets/puma.sock"
+# # Set up socket location
+# bind "unix://#{shared_dir}/sockets/puma.sock"
 
-# Logging
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+# # Logging
+# stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
-# Set master PID and state locations
-pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/pids/puma.state"
-activate_control_app
+# # Set master PID and state locations
+# pidfile "#{shared_dir}/pids/puma.pid"
+# state_path "#{shared_dir}/pids/puma.state"
+# activate_control_app
 
-on_worker_boot do
-  require "active_record"
-  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
-end
+# on_worker_boot do
+#   require "active_record"
+#   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+#   ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+# end
 
 # Specifies the `environment` that Puma will run in.
 #
